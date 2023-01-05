5304ae71e4d639a7bca2d8d741553b9e5e567130
DuyTrongk64 <75691078+DuyTrongk64@users.noreply.github.com>
2023-01-05 08:06:39 +0000

    update kadai1

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,19 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
-  # get_total_priceメソッドを定義してください
+    attr_accessor :name
+    attr_accessor :price
 
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+    def get_total_price(count)
+        total_price = count * self.price
+        if count >= 3 
+            total_price = total_price - 10000
+        end
+        return puts "#{self.name} #{total_price} vnd"
+    end
 end
 
 menu1 = Menu.new
-menu1.name = "Phở"
+menu1.name = "Pho"
 menu1.price = 30000
-
-# menu1に対してget_total_priceメソッドを呼び出してください
-
+puts "Nhap vao so bat pho: "
+count = gets.to_i
+menu1.get_total_price(count)
