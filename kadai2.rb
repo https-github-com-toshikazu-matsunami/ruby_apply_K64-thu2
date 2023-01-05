12306e2c3cf311d10d6b10a5ea0e81061bc8e69a
DuyTrongk64 <75691078+DuyTrongk64@users.noreply.github.com>
2023-01-05 08:12:26 +0000

    update kadai2

---
+++ kadai2.rb
@@ -0,0 +1,18 @@
+class Menu
+    attr_accessor :name
+    attr_accessor :price
+    def initialize(name:, price:)
+        self.name = name
+        self.price = price
+    end
+    def show_menu()
+        return "#{self.name} #{self.price}vnd"
+    end
+end
+menu1 = Menu.new(name: "Pho", price: 30000)
+menu2 = Menu.new(name: "Bun cha", price: 40000)
+menu3 = Menu.new(name: "Banh mi", price: 20000)
+menus = [menu1, menu2, menu3]
+menus.each do |menu|
+   puts menu.show_menu()
+end
